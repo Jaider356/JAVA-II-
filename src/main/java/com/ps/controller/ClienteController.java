900c6c4c5bc1301f3e28a545933d663667167b3f
jaider356 <jaiderdelgado59@gmail.com>
2024-03-27 11:42:42 -0500

    commit 1

--- src/main/java/com/ps/controller/ClienteController.java
+++ src/main/java/com/ps/controller/ClienteController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//TODO
+
 @RequiredArgsConstructor
 @RestController //Decir que esta clase es administrada o controlada por SPRING
 @RequestMapping("/clientes") //Agregar una ruta para mi clase
